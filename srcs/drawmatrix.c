9082d6189db911e7ebfb5f0313979c3d79e71e3f
Brianne Hugh beesbury <bhugh-be@e2r11p1.21-school.ru>
2019-04-10 17:23:52 +0300

    ok

--- srcs/drawmatrix.c
+++ srcs/drawmatrix.c
@@ -6,7 +6,7 @@
 /*   By: bhugh-be <bhugh-be@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/08 16:55:22 by bhugh-be          #+#    #+#             */
-/*   Updated: 2019/04/09 22:28:15 by bhugh-be         ###   ########.fr       */
+/*   Updated: 2019/04/10 17:23:27 by bhugh-be         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,23 @@
 
 void		rotate(t_line *line, int dx, int dy)
 {
-	line->y0 = line->y0 * cos(dy * R_KOEF) + line->z0 * sin(dy * R_KOEF);
-	line->z0 = -line->y0 * sin(dy * R_KOEF) + line->z0 * cos(dy * R_KOEF);
-	line->x0 = line->x0 * cos(dx * R_KOEF) + line->z0 * sin(dx * R_KOEF);
-	line->z0 = -line->x0 * sin(dx * R_KOEF) + line->z0 * cos(dx * R_KOEF);
-	line->y1 = line->y1 * cos(dy * R_KOEF) + line->z1 * sin(dy * R_KOEF);
-	line->z1 = -line->y1 * sin(dy * R_KOEF) + line->z1 * cos(dy * R_KOEF);
-	line->x1 = line->x1 * cos(dx * R_KOEF) + line->z1 * sin(dx * R_KOEF);
-	line->z1 = -line->x1 * sin(dx * R_KOEF) + line->z1 * cos(dx * R_KOEF);
+	int		d0[3];
+	int		d1[3];
+
+	d0[0] = line->x0;
+	d0[1] = line->y0;
+	d0[2] = line->z0;
+	d1[0] = line->x1;
+	d1[1] = line->y1;
+	d1[2] = line->z1;
+	line->y0 = (d0[1] * cos(dy * R_KOEF) + d0[2] * sin(dy * R_KOEF)) + 500;
+	d0[2] = (-d0[1] * sin(dy * R_KOEF) + d0[2] * cos(dy * R_KOEF));
+	line->x0 = (d0[0] * cos(dx * R_KOEF) + d0[2] * sin(dx * R_KOEF)) + 500;
+	line->z0 = (-d0[0] * sin(dx * R_KOEF) + d0[2] * cos(dx * R_KOEF)) + 500;	
+	line->y1 = (d1[1] * cos(dy * R_KOEF) + d1[2] * sin(dy * R_KOEF)) + 500;
+	d1[2] = (-d1[1] * sin(dy * R_KOEF) + d1[2] * cos(dy * R_KOEF));
+	line->x1 = (d1[0] * cos(dx * R_KOEF) + d1[2] * sin(dx * R_KOEF)) + 500;
+	line->z1 = (-d1[0] * sin(dx * R_KOEF) + d1[2] * cos(dx * R_KOEF)) + 500;
 }
 
 void		drawmatrix(t_values *values)
@@ -38,23 +47,27 @@ void		drawmatrix(t_values *values)
 		i = 0;
 		while (i < values->w)
 		{
-			line.x0 = i * SCALE + 6300;
-			line.y0 = j * SCALE + 6300;
+			line.x0 = (i - values->w / 2) * SCALE;
+			line.y0 = (j - values->h / 2)* SCALE;
 			line.z0 = values->dots[j][i].z;
 			line.c0 = values->dots[j][i].c;			
 			if (i + 1 < values->w)
 			{
-				line.x1 = (i + 1) * SCALE + 6300;
-				line.y1 = j * SCALE + 6300;
+				line.x1 = (i + 1 - values->w / 2) * SCALE;
+				line.y1 = (j - values->h / 2) * SCALE;
 				line.z1 = values->dots[j][i + 1].z;
 				line.c1 = values->dots[j][i + 1].c;
 				rotate(&line, values->dx, values->dy);
 				drawline(&line);
 			}
+			line.x0 = (i - values->w / 2) * SCALE;
+			line.y0 = (j - values->w / 2) * SCALE;
+			line.z0 = values->dots[j][i].z;
+			line.c0 = values->dots[j][i].c;	
 			if (j + 1 < values->h)
 			{
-				line.x1 = i * SCALE + 6300;
-				line.y1 = (j + 1) * SCALE + 6300;
+				line.x1 = (i - values->w / 2) * SCALE;
+				line.y1 = (j + 1 - values->h / 2) * SCALE;
 				line.z1 = values->dots[j + 1][i].z;
 				line.c1 = values->dots[j + 1][i].c;
 				rotate(&line, values->dx, values->dy);
